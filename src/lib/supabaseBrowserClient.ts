6dbaed820f0817d8da04224dfc1f5c0e08e48abe
David Ogden <DavidOgden112@gmail.com>
2025-06-30 12:24:28 +0100

    Help me

--- src/lib/supabaseBrowserClient.ts
+++ src/lib/supabaseBrowserClient.ts
@@ -1,6 +1,11 @@
 import { createBrowserClient } from '@supabase/ssr';
+import 'dotenv/config';
 
-export const supabase = createBrowserClient(
-	import.meta.env.SUPABASE_URL,
-	import.meta.env.SUPABASE_ANON_KEY
-);
+const supa_url = process.env.SUPABASE_URL;
+const supa_key = process.env.SUPABASE_ANON_KEY;
+
+if (!supa_key || !supa_url) {
+	throw new Error('Supabase env variables not set');
+}
+
+export const supabase = createBrowserClient(supa_url, supa_key);
--- src/lib/supabaseServerClient.ts
+++ src/lib/supabaseServerClient.ts
@@ -1,8 +1,16 @@
 import { createServerClient } from '@supabase/ssr';
 import type { RequestEvent } from '@sveltejs/kit';
+import 'dotenv/config';
+
+const supa_url = process.env.SUPABASE_URL;
+const supa_key = process.env.SUPABASE_ANON_KEY;
+
+if (!supa_key || !supa_url) {
+	throw new Error('Supabase env variables not set');
+}
 
 export function getSupabase(event: RequestEvent) {
-	return createServerClient(import.meta.env.SUPABASE_URL, import.meta.env.SUPABASE_ANON_KEY, {
+	return createServerClient(supa_url!, supa_key!, {
 		cookies: {
 			get: (name) => event.cookies.get(name),
 			set: (name, value, options) => event.cookies.set(name, value, { path: '/', ...options }),
