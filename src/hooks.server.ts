f08b29c7829e7d8da3de628cc9f0f38f4b277fbd
David Ogden <DavidOgden112@gmail.com>
2025-06-30 11:43:16 +0100

    Changed to get user as best practice

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,11 +3,15 @@ import { getSupabase } from '$lib/supabaseServerClient';
 
 export const handle: Handle = async ({event, resolve}) => {
     event.locals.supabase = getSupabase(event);
-    const {
-        data: { session }
-    } = await event.locals.supabase.auth.getSession();
-    event.locals.session = session;
-    event.locals.user = session?.user ?? null;
+  // Securely retrieve current user by revalidating session with Supabase Auth server
+  const {
+    data: { user },
+    error: userError
+  } = await event.locals.supabase.auth.getUser();
+  if (userError) {
+    console.error('Error in supabase.auth.getUser():', userError.message);
+  }
+  event.locals.user = user ?? null;
 
     const response = await resolve(event);
     return response;
