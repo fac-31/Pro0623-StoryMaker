77b1c1439baec7a9022e2882df6d305daa992b64
David Ogden <DavidOgden112@gmail.com>
2025-06-30 15:08:22 +0100

    Added histoire. Fixed .env bugs.

--- src/lib/supabaseBrowserClient.ts
+++ src/lib/supabaseBrowserClient.ts
@@ -1,11 +1,5 @@
 import { createBrowserClient } from '@supabase/ssr';
-import 'dotenv/config';
-
-const supa_url = process.env.SUPABASE_URL;
-const supa_key = process.env.SUPABASE_ANON_KEY;
-
-if (!supa_key || !supa_url) {
-	throw new Error('Supabase env variables not set');
-}
-
-export const supabase = createBrowserClient(supa_url, supa_key);
+export const supabase = createBrowserClient(
+	import.meta.env.VITE_SUPABASE_URL!,
+	import.meta.env.VITE_SUPABASE_ANON_KEY!
+);
