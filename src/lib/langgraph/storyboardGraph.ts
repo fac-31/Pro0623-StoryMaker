b90b2b75a8946c1b9d54a862aaeb1355164ca766
Joseph Potashnik <joseph.potashnik@gmail.com>
2025-07-01 13:32:57 +0100

    start flow working

--- src/lib/langgraph/storyboardGraph.ts
+++ src/lib/langgraph/storyboardGraph.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { StateGraph } from '@langchain/langgraph';
 import { ChatOpenAI } from '@langchain/openai';
 import { ChatPromptTemplate } from '@langchain/core/prompts';
